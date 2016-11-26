ef243eaacc802a69ab850956426b3ba6bc70fa68
Bart Kiers <bkiers@gmail.com>
2016-11-26 10:05:02 +0000

    pin first low then high then low

--- feeder/feeder.py
+++ feeder/feeder.py
@@ -4,6 +4,8 @@ import time
 def spin():
     gpio.setmode(gpio.BOARD)
     gpio.setup(22, gpio.OUT)
+    gpio.output(22, gpio.LOW)
+    time.sleep(0.1)
     gpio.output(22, gpio.HIGH)
     time.sleep(0.1)
     gpio.output(22, gpio.LOW)
