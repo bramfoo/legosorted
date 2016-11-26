6f07a54e781ecf46ef3c2fe1b99c069ecdfff087
bramfoo <bramlohman+git@gmail.com>
2016-11-26 01:03:25 +0100

    Added LED functionality when camera is active

--- camera/camera.py
+++ camera/camera.py
@@ -1,11 +1,21 @@
 from picamera import PiCamera
+from gpiozero import LED
 
+led = LED(18)
 
 def make_picture(file_name):
     try:
         camera = PiCamera()
         camera.resolution = (600, 400)
+        toggleLight(True)
         camera.capture(file_name)
+        toggleLight(False)
         return True
     except Exception:
         return False
+
+def toggleLight(self, status):
+    if (status):
+        led.on()
+    else:
+        led.off()
