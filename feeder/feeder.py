4779173ec49e6c792831da9d8c010d4d9412c713
Bart Kiers <bkiers@gmail.com>
2016-11-26 09:57:29 +0000

    pin number changed

--- feeder/feeder.py
+++ feeder/feeder.py
@@ -3,10 +3,10 @@ import time
 
 def spin():
     gpio.setmode(gpio.BOARD)
-    gpio.setup(18, gpio.OUT)
-    gpio.output(18, gpio.HIGH)
+    gpio.setup(22, gpio.OUT)
+    gpio.output(22, gpio.HIGH)
     time.sleep(0.1)
-    gpio.output(18, gpio.LOW)
+    gpio.output(22, gpio.LOW)
 
 if __name__ == "__main__":
     spin()
