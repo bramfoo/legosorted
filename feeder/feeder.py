f2953c81eca92f23b66b88a0703fd9cd56998ca9
Bart Kiers <bkiers@gmail.com>
2016-11-26 09:37:57 +0000

    added spin for feeder

--- feeder/feeder.py
+++ feeder/feeder.py
@@ -1,6 +1,12 @@
-def spin():
-    print "spinning..."
+import RPi.GPIO as gpio
+import time
 
+def spin():
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(18, gpio.OUT)
+    gpio.output(18, gpio.HIGH)
+    time.sleep(0.1)
+    gpio.output(18, gpio.LOW)
 
 if __name__ == "__main__":
     spin()
